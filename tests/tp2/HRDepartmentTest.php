fce2e4d74b602719303ff455163fe9b539aa004d
Frederic mayor <fredomay@gmail.com>
2015-09-09 17:28:19 +0200

    test HRDepartementTest.php ok

--- tests/tp2/HRDepartmentTest.php
+++ tests/tp2/HRDepartmentTest.php
@@ -9,4 +9,39 @@ use tp2\HRDepartment;
 class HRDepartmentTest extends \PHPUnit_Framework_TestCase
 {
 
+    protected $person;
+    protected $hrdepartment;
+    protected $enterprise;
+
+    public function setUp()
+    {
+        $this->person = New Person('fred');
+        $this->enterprise = \Phake::mock('tp2/Enterprise');
+        $this->hrdepartment = new HRDepartment($this->enterprise);
+        
+    }
+
+    public function testHire()
+    {
+        \Phake::when($this->enterprise)->employ($this->person)->thenReturn(false);
+        $this->hrdepartement->hire($this->person);
+        \Phake::verify($this->enterprise)->add($this->person);
+    }
+
+
+    public function testFire()
+    {
+        \Phake::when($this->enterprise)->employ($this->person)->thenReturn(true);
+        $this->hrdepartement->fire($this->person);
+        \Phake::verify($this->enterprise)->remove($this->person);
+
+    }
+
+    public function testIsEmployee()
+    {
+         Phake::when($this->enterprise)->employ($this->person)->thenReturn(true);
+         Phake::when($this->enterprise)->employ($this->person)->thenReturn(false);
+    }
+
+
 }
