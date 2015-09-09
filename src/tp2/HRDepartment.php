793fc0207a542f3d450a50f5a001649aca117271
Frederic mayor <fredomay@gmail.com>
2015-09-09 17:50:59 +0200

    HRDepartement ok

--- src/tp2/HRDepartment.php
+++ src/tp2/HRDepartment.php
@@ -4,35 +4,43 @@ namespace tp2;
 
 use tp2\Person;
 use tp2\Enterprise;
+use tp2\Exception\AlreadyEmployedException;
+use tp2\Exception\NoEmployedException;
 
 class HRDepartment
 {
+    protected $entreprise;
     public function __construct(Enterprise $entreprise)
     {
-        // TO IMPLEMENT
+        $this->entreprise = $entreprise;
     }
-
     /**
      * @throws \tp2\Exception\AlreadyEmployedException When the given person is already an employee
      */
     public function hire(Person $person)
     {
-        // TO IMPLEMENT
+        if($this->entreprise->employ($person) === true)
+        {
+            throw new AlreadyEmployedException('the given person is already an employee');
+        }
+        $this->entreprise->add($person);
     }
-
     /**
      * @throws \tp2\Exception\NoEmployedException When the given person is not an employee
      */
     public function fire(Person $person)
     {
-        // TO IMPLEMENT
+        if($this->entreprise->employ($person) === false)
+        {
+            throw new NoEmployedException('the given person is not an employee');
+        }
+        $this->entreprise->remove($person);
     }
-
     /**
      * @return boolean
      */
     public function isEmployee(Person $person)
     {
-        // TO IMPLEMENT
+       return $this->entreprise->employ($person);
     }
 }
--- tests/tp2/HRDepartmentTest.php
+++ tests/tp2/HRDepartmentTest.php
@@ -18,7 +18,6 @@ class HRDepartmentTest extends \PHPUnit_Framework_TestCase
         $this->person = New Person('fred');
         $this->enterprise = \Phake::mock('tp2/Enterprise');
         $this->hrdepartment = new HRDepartment($this->enterprise);
-        
     }
 
     public function testHire()
@@ -28,19 +27,34 @@ class HRDepartmentTest extends \PHPUnit_Framework_TestCase
         \Phake::verify($this->enterprise)->add($this->person);
     }
 
+    public function testHireException()
+    {
+        \Phake::when($this->enterprise)->employ($this->person)->thenReturn(true);
+        $this->setExpectedException('tp2\Exception\AlreadyEmployedException');
+        $this->hrDepartement->hire($this->person);
+    }
 
     public function testFire()
     {
         \Phake::when($this->enterprise)->employ($this->person)->thenReturn(true);
         $this->hrdepartement->fire($this->person);
         \Phake::verify($this->enterprise)->remove($this->person);
+    }
 
+    public function testFireException()
+    {
+        \Phake::when($this->enterprise)->employ($this->person)->thenReturn(false);
+        $this->setExpectedException('tp2\Exception\NoEmployedException');
+        $this->hrDepartement->fire($this->person);
     }
 
     public function testIsEmployee()
     {
          Phake::when($this->enterprise)->employ($this->person)->thenReturn(true);
+         $this->assertTrue($this->hrdepartement->isEmployee($this->person));
+
          Phake::when($this->enterprise)->employ($this->person)->thenReturn(false);
+         $this->assertFalse($this->hrdepartement->isEmployee($this->person));
     }
 
 
